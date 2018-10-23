7f34f56be94d5093ebf8e2915ea467b02f45e71a
flymanflyman <406544970@qq.com>
2018-10-23 16:46:50 +0800

    20181023 from home.

--- src/main/java/com/lh/servicezuul/filter/BlackNameFilter.java
+++ src/main/java/com/lh/servicezuul/filter/BlackNameFilter.java
@@ -1,11 +1,9 @@
 package com.lh.servicezuul.filter;
 
 import com.lh.servicezuul.model.ReturnModel;
-import com.lh.servicezuul.myClass.IsCheckWhitePath;
 import com.lh.servicezuul.myClass.MyBlackNameList;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
-import com.netflix.zuul.exception.ZuulException;
 
 import javax.servlet.http.HttpServletRequest;
 
