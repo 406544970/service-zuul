9a3b8ea9c9129f27a9babf7d943da61d13579c84
406544970 <406544970@qq.com>
2018-12-23 13:03:54 +0800

    2018.12.13 from company.

--- src/main/java/com/lh/servicezuul/myclass/CheckAccessTokenClass.java
+++ src/main/java/com/lh/servicezuul/myclass/CheckAccessTokenClass.java
@@ -1,6 +1,9 @@
 package com.lh.servicezuul.myclass;
 
 import com.lh.servicezuul.model.TokenClass;
+import com.lh.servicezuul.service.IdentityService;
+import com.lishunyi.result.VO.ResultVO;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * @author 梁昊
@@ -13,14 +16,34 @@ public class CheckAccessTokenClass {
         super();
     }
 
+    @Autowired
+    private IdentityService identityService;
+
     public boolean isAccessTokenOk(TokenClass tokenClass) {
+        boolean isOk = false;
+        ResultVO resultVO = new ResultVO();
         String useId = tokenClass.getUseId();
         String accessToken = tokenClass.getAccessToken();
         String useType = tokenClass.getUseType();
         if ((useId != null) && (accessToken != null) && (useType != null)) {
-            return true;
-        } else {
-            return false;
+            switch (useType) {
+                case "Manager":
+                    resultVO = identityService.checkManagerIdentity(useId, accessToken);
+                    break;
+                case "pctourist":
+                    resultVO = identityService.checkPcTouristsIdentity(useId, accessToken);
+                    break;
+                case "apptourist":
+                    resultVO = identityService.checkAppTouristsIdentity(useId, accessToken);
+                    break;
+                case "weixintourist":
+                    resultVO = identityService.checkWeiXinTouristsIdentity(useId, accessToken);
+                    break;
+                default:
+                    break;
+            }
+            isOk = resultVO.getCode().equals("200") ? true : false;
         }
+        return isOk;
     }
 }
